72cf6f3a91bc4feb5ffd7824459623152ab1d0d0
FrankHu <frank.hu@perficient.com>
2020-08-17 16:33:04 +0800

    debounce

---
+++ anki/函数防抖.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => {
+  console.log(a + b);
+};
+
+let sumDelay = debounce(sum, 1000);
+sumDelay(1, 1);
+sumDelay(2, 2);
+sumDelay(3, 3);
+sumDelay(4, 4);
+
+setTimeout(() => sumDelay(5, 5), 2000);
